4f581d760d59f23f554eea358e261f4d77396243
Stefan_Karlsson <gittest@k-son.se>
2020-09-08 11:47:06 +0200

    Start på GL-klassen

--- rekurs.py
+++ rekurs.py
@@ -13,3 +13,43 @@ def dotprod(a,b):
 
 print(dotprod([1,2,3],[2,-1,1]))
 
+def matrixtranspose(a):
+    # A matrix is is represented as a list of lists
+    # A column of rows
+    # a[r][c]
+    m = len(a)
+    n = len(a[0])
+    at = [[]]*n
+    for row in a:
+        i = 0
+        for elem in row:
+            at[i].append(elem)
+            i+=1
+    return at
+
+def matrixmult(a,b):
+    # A matrix is is represented as a list of lists
+    # A column of rows
+    # a[r][c]
+    bt = matrixtranspose(a)
+    c = []
+    for arow in a:
+        crow = []
+        for bcol in bt:
+            crow.append(dotprod(arow,bcol))
+        c.append(crow)
+    return c
+
+def printmatrix(a):
+    for row in a:
+        prow = ""
+        for elem in row:
+            prow += str(elem)+"\t"
+        print(prow)
+
+
+a=[[1,2],[3,4]]
+b=[[7,-8],[-9,10]]
+printmatrix(a)
+printmatrix(b)
+printmatrix(matrixmult(a,b))
