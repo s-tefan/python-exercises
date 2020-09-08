b1bca12b8d40182a9142cf84c7d75c022e635171
Stefan_Karlsson <gittest@k-son.se>
2020-09-08 11:47:35 +0200

    Start på GL-klassen

---
+++ gl.py
@@ -0,0 +1,29 @@
+class GL:
+    def __init__(self, p, poly, gen):
+        self.p = p # Should check for primality
+        self.poly = poly # Should check for irreducibility
+        self.gen = gen # Should check for primitiveness
+
+    def genpow(self, n):
+        if n == 0:
+            return [1]
+        elif n==1:
+            return self.gen
+        elif n%2 == 0:
+            he = self.genpow(n//2)
+            return self.prod(he,he) 
+        else:
+            return self.prod(self.genpow(n-1),self.gen)
+
+    def sum(self,a,b):
+        % TBI sum of two elements
+        pass
+
+    def prod(self,a,b):
+        % TBI product of two elements
+        pass
+        
+        
+
+
+
