35b25307c4f067e797d3a7990a9922621951a5e4
Stefan_Karlsson <gittest@k-son.se>
2020-09-10 17:30:35 +0200

    GF

--- gf.py
+++ gf.py
@@ -1,4 +1,4 @@
-class GL:
+class GF:
     def __init__(self, p, poly, gen):
         self.p = p # Should check for primality
         self.poly = poly # Should check for irreducibility
@@ -15,15 +15,22 @@ class GL:
         else:
             return self.prod(self.genpow(n-1),self.gen)
 
-    def sum(self,a,b):
+    @classmethod
+    def sum(self,a, b):
         % TBI sum of two elements
         pass
 
+    @classmethod
     def prod(self,a,b):
         % TBI product of two elements
         pass
         
         
+class GFElement:
+    def __init__(self, field, coeffs=None, power=None):
+        self.field = field: GF
+        
 
+    
 
 
