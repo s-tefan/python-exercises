bd49b975382ca1a56559204d60fdf6960a68e9d1
Stefan K <40999112+s-tefan@users.noreply.github.com>
2021-10-20 03:24:34 +0200

    -

---
+++ python3.10test.py
@@ -0,0 +1,22 @@
+# python 3.10
+
+def apa():
+    while True:
+        x = input()
+        match x.split():
+            case [apa, '+', bepa]:
+                print(eval(apa)+eval(bepa))
+            
+def bepa():
+    for x in ["apa > bepa", "apa", "apa > bepa cepa", "apa bepa > cepa depa"]:
+        match x.split():
+            case [ap, '>', bep]:
+                print("{0} implies {1}".format(ap, bep))
+            case [ap, '>', *bep]:
+                print("{0} implies {1}".format(ap, bep))
+            case [*ap, '>', bep]:
+                print("{0} implies {1}".format(ap, bep))
+            case [*ap, '>', *bep]:
+                print("{0} implies {1}".format(ap, bep))
+
+bepa()
