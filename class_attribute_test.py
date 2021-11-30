ea2b8b6a16fecbbba6e16837512cd83d494d4c69
s-tefan <github@k-son.se>
2021-11-30 08:50:14 +0100

    -

--- class_attribute_test.py
+++ class_attribute_test.py
@@ -6,17 +6,18 @@ if not found then the namespace for its class.
 
 
 class Apa:
-    ap = 23
+    ap = 23 # sets a class attribute
+
     def __init__(self):
-        self.bep = 42
-    def change(self, x):
-        self.ap = x
-    def cls_change(self, x):
-        type(self).ap = x
+        self.bep = 42 # sets an instance attribute 'bep'
+    def set(self, x):
+        self.ap = x # sets the instance attribute 'ap'
+    def cls_set(self, x):
+        type(self).ap = x # sets the class attribute 'Apa.ap'
     
 apa = Apa()
-print(apa.ap, apa.bep, type(apa).ap)
-apa.change(97)
-print(apa.ap, apa.bep, type(apa).ap)
-apa.cls_change(93)
+print(apa.ap, apa.bep, type(apa).ap) # As 'apa.ap' is not set as an instance attribute, the class attribute value 'Apa.ap' is used
+apa.set(97) # Sets the instance attribute 'apa.ap' 
+print(apa.ap, apa.bep, type(apa).ap) # Now 'apa.ap' is a distinct attribute
+apa.cls_set(93) # Sets the class attribute 'Apa.ap'
 print(apa.ap, apa.bep, type(apa).ap)
