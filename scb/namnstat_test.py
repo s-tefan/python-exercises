ecd234730a00ba721a7f612383d0ce3b3c6914c9
s-tefan <github@k-son.se>
2022-11-14 18:41:47 +0100

    funkis, mer kommenterat

--- scb/namnstat_test.py
+++ scb/namnstat_test.py
@@ -1,12 +1,16 @@
 from pyscbwrapper import SCB
 
 scb = SCB('sv')
+# Vi går ner genom trädet
 scb.go_down('BE')
 scb.go_down('BE0001')
 scb.go_down('BE0001G')
+# För nodinformation:
+# scb.info()
 scb.go_down('BE0001T06AR')
 # alternativt, kortare:
 # scb = SCB('sv','BE','BE0001','BE0001G','BE0001T06AR')
+# Nu är vi nere på lövnivå och kan hämta grejer
 vars = scb.get_variables()
 scb.set_query(tilltalsnamn = 'Stefan', tabellinnehåll = 'Antal bärare', år = '2021')
 scb.get_query()
